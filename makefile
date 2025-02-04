dfd319ee23dcea380558a9e7f28f360dba88bf78
Conner Tenn <connertenn@gmail.com>
2025-02-03 20:51:44 -0500

    Single LED lit

--- makefile
+++ makefile
@@ -3,9 +3,13 @@ PROJECT_NAME := led-controller
 BUILD_DIR := ${CURDIR}/build
 
 PICO_TARGET := rp2350
-EXTRA_LIB_DEPENDENCIES := 
+EXTRA_LIB_DEPENDENCIES := ${BUILD_DIR}/ws2812.pio.h
 
 include Pico-Zig/rules.mk
+
+
+${BUILD_DIR}/ws2812.pio.h: pico-sdk Pico-Zig/library/ws2812.pio | ${BUILD_DIR}
+	@cd $(BUILD_DIR) && cmake .. -DPICO_SDK_PATH=${RUN_DIR}/pico-sdk && make -j 20 ${PROJECT_NAME}_ws2812_pio_h
 
 
 # == Repos ==
--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,7 @@ const pico = @import("pico");
 const csdk = pico.csdk;
 const stdio = pico.stdio;
 const hardware = pico.hardware;
+const WS2812 = pico.library.WS2812;
 
 export fn main() void {
     //Init prints
@@ -17,6 +18,15 @@ export fn main() void {
 
     csdk.sleep_ms(2000);
     stdio.print("== Led Controller ==\n", .{});
+
+    var ws2812 = WS2812.WS2812.create(hardware.gpio.Pin.create(0)) catch |err| {
+        stdio.print("Failed to initialize ws2812: {}", .{err});
+        return;
+    };
+    ws2812.init();
+
+    stdio.print("Put pixel\n", .{});
+    ws2812.putPixel(WS2812.Pixel.create(255, 255, 0, 0));
 
     var toggle = true;
     while (true) {
