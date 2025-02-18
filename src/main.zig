25b4e7f6f8238d85f10f4e6700d4cb6abf4e1bf9
Conner Tenn <connertenn@gmail.com>
2025-02-18 00:39:23 -0500

    Improved the demo with button states and simple motor driving

--- src/main.zig
+++ src/main.zig
@@ -24,11 +24,21 @@ export fn main() void {
     //== Setup Objects ==
     //Buttons
     const button_up = hardware.gpio.Pin.create(28);
-    _ = button_up; // autofix
     const button_center = hardware.gpio.Pin.create(26);
-    _ = button_center; // autofix
     const button_down = hardware.gpio.Pin.create(27);
-    _ = button_down; // autofix
+
+    button_up.init(hardware.gpio.Pin.Config{
+        .direction = .in,
+        .pull = .{ .up = false, .down = true },
+    });
+    button_center.init(hardware.gpio.Pin.Config{
+        .direction = .in,
+        .pull = .{ .up = false, .down = true },
+    });
+    button_down.init(hardware.gpio.Pin.Config{
+        .direction = .in,
+        .pull = .{ .up = false, .down = true },
+    });
 
     //LED controller
     stdio.print("Create Led Strip\n", .{});
@@ -44,7 +54,32 @@ export fn main() void {
         stdio.print("Error:{}\n", .{err});
         return;
     };
+
+    const sensor = duty_cycle_sampler.getSensor();
+
+    const pid = pico.library.pid.PIDcontrol.create(
+        3.0,
+        0.0,
+        0.05,
+    );
+
+    csdk.gpio_set_function(10, csdk.GPIO_FUNC_PWM); //UL
+    csdk.gpio_set_function(11, csdk.GPIO_FUNC_PWM); //UH
+    csdk.gpio_set_function(12, csdk.GPIO_FUNC_PWM); //VL
+    csdk.gpio_set_function(13, csdk.GPIO_FUNC_PWM); //VH
+    csdk.gpio_set_function(14, csdk.GPIO_FUNC_PWM); //WL
+    csdk.gpio_set_function(15, csdk.GPIO_FUNC_PWM); //WH
+    var motor = pico.library.motor.Motor.create(
+        6,
+        7,
+        5,
+        7,
+        sensor,
+        pid,
+    );
+
     duty_cycle_sampler.init();
+    motor.init();
 
     //Display
     var display = pico.library.gu128x32.GU128x32.create(
@@ -111,8 +146,51 @@ export fn main() void {
     // display.display_buffer.display_buffer[1][7] = 0b01001001;
     // display.display_buffer.display_buffer[1][8] = 0b01000101;
     // display.display_buffer.display_buffer[1][9] = 0b00111110;
-    // display.display_buffer.print("R: {d: >3}", .{0}, 3, 0);
-    // display.render();
+    display.display_buffer.print("Zeroing motor...", .{}, 0, 0);
+    display.render();
+
+    for (0..200) |sample_idx| {
+        //Drive to the target angle
+        const target_angle = tau * @as(f32, @floatFromInt(sample_idx)) / 200.0;
+
+        motor.setTorque(1.0, 0.0, target_angle);
+        csdk.sleep_ms(10);
+    }
+
+    const zeroMotor = struct {
+        const dead_zone = 0.02;
+        var zeroed = false;
+
+        fn targetAngle(angle: f32, delta_time_s: f32) f32 {
+            _ = delta_time_s; // autofix
+            const delta_err = pico.math.deltaError(f32, angle, 0, tau);
+            stdio.print("angle: {d:.3}  delta_err: {d:.3}\n", .{ angle, delta_err });
+
+            if (@abs(delta_err) < dead_zone) {
+                zeroed = true;
+                return 0;
+            }
+            return -math.sign(delta_err) * 0.3;
+        }
+
+        fn zeroMotor(motor_obj: *pico.library.motor.Motor) void {
+            while (!zeroed) {
+                motor_obj.update(targetAngle);
+            }
+        }
+    }.zeroMotor;
+    zeroMotor(&motor);
+    motor.setTorque(1.0, 0.0, 0.0);
+
+    // var angle: f32 = 0;
+    // while (true) {
+    //     motor.setTorque(1.0, 0.0, angle);
+    //     angle += 0.0001 * tau;
+    //     csdk.sleep_ms(1);
+    // }
+
+    display.display_buffer.clear();
+    display.render();
 
     //== Loop ==
     stdio.print("== Loop ==\n", .{});
@@ -135,10 +213,16 @@ export fn main() void {
 
         led_strip.render();
 
-        display.display_buffer.print("R: {d: >3}", .{pixel_colour.rgbw.red}, 0, 0);
-        display.display_buffer.print("G: {d: >3}", .{pixel_colour.rgbw.green}, 1, 0);
-        display.display_buffer.print("B: {d: >3}", .{pixel_colour.rgbw.blue}, 2, 0);
-        display.display_buffer.print("W: {d: >3}", .{pixel_colour.rgbw.white}, 3, 0);
+        // display.display_buffer.print("R: {d: >3}", .{pixel_colour.rgbw.red}, 0, 0);
+        // display.display_buffer.print("G: {d: >3}", .{pixel_colour.rgbw.green}, 1, 0);
+        // display.display_buffer.print("B: {d: >3}", .{pixel_colour.rgbw.blue}, 2, 0);
+        // display.display_buffer.print("W: {d: >3}", .{pixel_colour.rgbw.white}, 3, 0);
+        display.display_buffer.print("Hue: {d: <.3}", .{hsv.hue}, 0, 0);
+        display.display_buffer.print("B1:{s}  B2:{s}  B3:{s}", .{
+            if (!button_up.get()) "O" else "X",
+            if (!button_center.get()) "O" else "X",
+            if (!button_down.get()) "O" else "X",
+        }, 3, 0);
         display.render();
     }
 
